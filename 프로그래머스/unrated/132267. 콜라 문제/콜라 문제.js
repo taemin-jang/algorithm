1e02432744372a10d0ead4b2a59996814f656d3b
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-01-17 14:54:14 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,22 +1,8 @@
 function solution(a, b, n) {
     let result = 0;
-    let service = 0;
-    
-    while(true){
-        if(n % a === 0){
-            service = n / a;
-            result += service * b;
-            n = n - (service * a) + (service * b);
-        }
-        if(n % a < a){
-            // 정수 만들기 틸트 연산자
-            service = ~~(n / a);
-            if(service === 0){
-                break;
-            }
-            result += service * b;
-            n = n - (service * a) + (service * b);
-        }
+    while(n >= a){
+            result += ~~(n / a) * b;
+            n = n - (~~(n / a) * a) + (~~(n / a) * b);
     }
     return result;
 }
