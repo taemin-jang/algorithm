6202e2f5834a3159bf2a553995071b3e3d76fcd5
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-01-27 13:52:54 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.39 ms
+메모리: 33.6 MB, 시간: 0.28 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,16 +1,20 @@
 function solution(bridge_length, weight, truck_weights) {
+    let truckWeights = [...truck_weights];
     let bridgeArray = []
-    let count = 0;
-    let n = truck_weights.length;
     let answer = [];
-    while(answer.length !== n){
+    let count = 0;
+    while(answer.length !== truck_weights.length){
+        // 다리를 건너는 트럭의 수가 꽉 찼을 때
         if(bridgeArray.length === bridge_length){
             let truck = bridgeArray.pop();
+            // 마지막에 있는 트럭이 0이 아닐경우 다리를 지난 트럭에 넣어준다.
             if(truck) answer.push(truck);
         }
-        
-        if(bridgeArray.reduce((acc,cur) => acc + cur, 0) + truck_weights[0] <= weight){
-            bridgeArray.unshift(truck_weights.shift());
+        // 다리를 건너는 트럭 무게의 합과 이번에 다리를 건널 트럭 무게의 합이 
+        // 다리가 견딜 수 있는 무게보다 작을 경우
+        // bridgeArray에 추가해주고 아니면 0을 추가해준다.
+        if(bridgeArray.reduce((acc,cur) => acc + cur, 0) + truckWeights[0] <= weight){
+            bridgeArray.unshift(truckWeights.shift());
         }else{
             bridgeArray.unshift(0);
         }
