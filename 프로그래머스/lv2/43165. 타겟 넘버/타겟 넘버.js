b25effb23797dadaa0097219babb5c7a7c359027
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-01-31 14:46:44 +0900

    [level 2] Title: 타겟 넘버, Time: 1.43 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,16 @@
+function solution(numbers, target) {
+    let answer = 0;
+    function dfs(level, sum) {
+        if(level === numbers.length){
+            if(sum === target){
+                answer++;
+            }
+            return;
+        }
+        dfs(level + 1, sum + numbers[level]);
+        dfs(level + 1, sum - numbers[level]);
+    }
+    dfs(0,0)
+    return answer;
+}
+
