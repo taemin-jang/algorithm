e0704e2608a57dde31d6891efcbbef5f8318f002
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-02-03 17:57:32 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    const numbers = {
+        'zero' : 0,
+        'one' : 1,
+        'two' : 2,
+        'three' : 3,
+        'four' : 4,
+        'five' : 5,
+        'six' : 6,
+        'seven' : 7,
+        'eight' : 8,
+        'nine' : 9,
+    }
+    for(v in numbers){
+        if(s.includes(v)) s = s.replaceAll(v, numbers[v]);
+    }
+    return Number(s);
+}
