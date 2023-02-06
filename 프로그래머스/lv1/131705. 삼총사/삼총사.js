f074684eae69300a81ec68df6da63dc6502d72d2
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-02-06 10:31:43 +0900

    [level 1] Title: 삼총사, Time: 4.67 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,15 +1,16 @@
 function solution(number) {
     let answer = [];
-    const dfs = (nums, num, arr = []) => {
+    const permutation = (nums, num, arr = []) => {
         if(num === 3) answer.push([...arr]);
         else {
             for(let i = 0; i < nums.length; i++) {
                 arr.push(nums[i]);
-                dfs(nums.slice(i + 1), num + 1, arr);
+                permutation(nums.slice(i + 1), num + 1, arr);
                 arr.pop();
             }
         }
     }
-    dfs(number, 0);
+    permutation(number, 0);
+    console.log(answer)
     return answer.filter(v => v.reduce((arr,cur) => arr + cur,0) === 0).length;
 }
