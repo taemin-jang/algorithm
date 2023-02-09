265dca23458dcb3bccfe0b2c21d966b3bd2a9f62
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-02-10 00:38:34 +0900

    [level 1] Title: 체육복, Time: 0.11 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/README.md
+++ 프로그래머스/lv1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.10 ms
+메모리: 33.7 MB, 시간: 0.11 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -2,17 +2,16 @@ function solution(n, lost, reserve) {
     let arr = new Set(reserve.slice());
     let cnt = 0;
     lost = lost.filter(v => {
+        // 만약 잃어버린 학생 중 여벌이 있는 학생은 자기가 써야하므로
+        // arr에서 빼주고 오름차순 정렬했다.
         if(arr.has(v)){
             arr.delete(v);
         }else{
             return v;
         }
     }).sort((a,b) => a - b);
+    
     for(let i = 0; i < lost.length; i++){
-        if(arr.has(lost[i])){
-            arr.delete(lost[i])
-            continue;
-        }
         if(arr.has(lost[i]-1)) {
             arr.delete(lost[i]-1)
             continue;
