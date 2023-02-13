765bcafa8c4d7c268d51452aab705174b89f8622
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-02-13 17:28:35 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.66 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,7 +1,9 @@
 function solution(n, left, right) {
     let arr = [];
     for(let i = left; i<=right; i++){
+        // i의 대한 좌표값 구하기
         const [x,y] = [~~(i/n), i % n];
+        // 해당 좌표에서 가장 큰 값에 + 1해주면 n 행 또는 n 열의 값이 나옴
         arr.push(Math.max(x,y)+1)
     }
     return arr;
