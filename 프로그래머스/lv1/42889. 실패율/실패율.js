043d72e298bc2d2a1b0f52c68cd6aeb6b910beb0
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-02-14 22:07:27 +0900

    [level 1] Title: 실패율, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -5,11 +5,5 @@ function solution(N, stages) {
         let notClear = stages.filter(v => v === i).length;
         answer[i] = notClear / stages.filter(v => v >= i).length;
     }
-    return Object.entries(answer).sort((a,b) => {
-        if(b[1] > a[1]) return 1;
-        if(b[1] < a[1]) return -1;
-        if(b[1] === a[1]) {
-            if(b[0] < a[0]) return 0;
-        }
-    }).map(v => +v[0]);
+    return Object.entries(answer).sort((a,b) =>b[1] - a[1]).map(v => +v[0]);
 }
