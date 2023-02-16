5a1014ac1fa51048f77cec98cd90c4cf45ad179f
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-02-16 17:23:32 +0900

    [unrated] Title: 카드 뭉치, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,13 +1,9 @@
 function solution(cards1, cards2, goal) {
     for(let v of goal) {
         if(cards1.includes(v)){
-            if(cards1.shift() !== v){
-                return "No";
-            }
+            if(cards1.shift() !== v) return "No";
         }else{
-            if(cards2.shift() !== v){
-                return "No";
-            }
+            if(cards2.shift() !== v) return "No";
         }
     }
     return "Yes";
