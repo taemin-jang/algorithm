717d2242a097ffa46affd44c6df328fe97af9753
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-02-21 14:36:34 +0900

    [level 1] Title: 소수 찾기, Time: 59.22 ms, Memory: 45.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    const arr = Array(n+1).fill(1);
+    arr[0] = 0;
+    arr[1] = 0;
+    for(let i = 2; i < n; i++){
+        let j = 2;
+        while(i*j <= n){
+          arr[i*j] = 0;  
+          j++;
+        }
+    }
+    console.log(arr)
+    return arr.filter(v => v).length;
+}
