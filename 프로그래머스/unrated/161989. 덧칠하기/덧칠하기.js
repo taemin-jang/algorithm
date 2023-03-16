02cd22c269206f972a1954582cad5d18babd9f46
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-03-16 23:37:12 +0900

    [unrated] Title: 덧칠하기, Time: 1.32 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -1,17 +1,3 @@
-// function solution(n, m, section) {
-//     let paint = Array(n+1).fill(0).map((v,i) => section.includes(i) ? 1 : v);
-//     let cnt = 0;
-    
-//     while(paint.includes(1)){
-//         const index = paint.indexOf(1);
-//         for(let j = 0; j < m; j++){
-//             if(paint[index+j]) paint[index+j] = 0;
-//         }
-//         cnt++;
-//     }
-//     return cnt;
-// }
-
 function solution(n, m, section) {
   let answer = 0;
   let max = 0;
