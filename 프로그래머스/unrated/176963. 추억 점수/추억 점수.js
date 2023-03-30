6cae4ccf065d278051b946a531fa92faa2bf16b0
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-03-30 22:11:13 +0900

    [unrated] Title: 추억 점수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,7 @@
+function solution(name, yearning, photo) {
+    let obj = {};
+    for(let i = 0; i < name.length; i++){
+        obj[name[i]] = yearning[i];
+    }
+    return photo.map(value => value.map(v => obj[v] ? obj[v] : 0).reduce((acc,cur) => acc + cur,0))
+}
