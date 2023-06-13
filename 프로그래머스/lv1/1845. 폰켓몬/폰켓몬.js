4b96fb930927ee558bf2b05dda36fdd0b187656f
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-06-13 19:41:28 +0900

    [level 1] Title: 폰켓몬, Time: 0.25 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,4 +1,4 @@
 function solution(nums) {
-    let set = new Set(nums);
-    return set.size <= parseInt(nums.length/2) ? set.size : parseInt(nums.length/2);
+    const set = new Set(nums);
+    return (nums.length / 2) > set.size ? set.size : nums.length / 2;
 }
