adcfd52e003cbb9735ae3ab0f51e0bc4002ad813
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-06-15 13:25:57 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/README.md
+++ 프로그래머스/lv2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.07 ms
+메모리: 33.5 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,16 +1,19 @@
 function solution(progresses, speeds) {
-    const deploy = progresses.map((v,i) => Math.ceil((100 - v)/speeds[i]));
-    const result = [];
-    let cnt = 1
-    let max = deploy[0];
-    deploy.forEach((v,i) => {
-        if(max < deploy[i+1] || !deploy[i+1]){
-            result.push(cnt);
-            cnt = 1;
-            max = deploy[i+1];
-        }else{
-            cnt++;
+    progresses = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
+    let max = 0;
+    let count = 1;
+    let result = [];
+    progresses.forEach(progress => {
+        if(!max) max = progress;
+        else {
+            if(progress <= max) count++;
+            else {
+                max = progress;
+                result.push(count);
+                count = 1;
+            }
         }
     })
+    result.push(count)
     return result;
 }
