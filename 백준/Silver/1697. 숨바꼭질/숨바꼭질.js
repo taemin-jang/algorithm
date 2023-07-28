ef62a4bb4e9bbf52653c84d91f810d760f3c6fda
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2023-07-28 14:51:56 +0900

    [Silver I] Title: 숨바꼭질, Time: 5972 ms, Memory: 24232 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -3,17 +3,19 @@ let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let [N, K] = input[0].trim().split(" ").map(Number);
 let chk = Array(100001).fill(false);
-const getFastestFindTime = (start, end) => {
-  const queue = [[start, 0]];
-  while (queue.length) {
-    const [pos, time] = queue.shift();
+const bfs = (n, t) => {
+  let q = [];
+  q.push([n, t]);
+  while (q.length) {
+    let [pos, time] = q.shift();
     if (chk[pos]) continue;
-    if (pos === end) return time;
-    for (const move of [pos + 1, pos - 1, pos * 2]) {
-      !chk[move] && move >= 0 && move <= 100000 && queue.push([move, time + 1]);
+    if (pos === K) return time;
+    for (let nx of [pos - 1, pos + 1, pos * 2]) {
+      if (nx >= 0 && nx <= 100000 && chk[nx] === false) {
+        q.push([nx, time + 1]);
+      }
     }
-    chk[pos] = 1;
+    chk[pos] = true;
   }
 };
-
-console.log(getFastestFindTime(N, K));
+console.log(bfs(N, 0));
