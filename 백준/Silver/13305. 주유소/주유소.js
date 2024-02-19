10fb5b44b54f78d32b9b22fbd48c46e5246de67d
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2024-02-19 10:54:48 +0900

    [Silver III] Title: 주유소, Time: 296 ms, Memory: 36636 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -3,20 +3,14 @@ let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 let T = +input[0];
 let city = input[1].split(" ").map(Number);
-let oils = input[2].split(" ").slice(0, input[2].split(" ").length - 1).map(Number);
-
-let restDistance = city.reduce((acc, cur) => acc + cur,0);
-let minOil = Math.min(...oils);
+let oils = input[2].split(" ").map(Number);
+let minOil = oils[0];
+for(let i = 0; i < oils.length; i++){
+  minOil = Math.min(oils[i], minOil);
+  oils[i] = minOil;
+}
 let result = BigInt(0);
-let i = 0;
-let o = BigInt(10001);
-for(let oil of oils){
-  if(oil < o){
-    result += BigInt(oil) * BigInt(city[i]);
-    o = BigInt(oil);
-  }else{
-    result += BigInt(o) * BigInt(city[i]);
-  }
-    i++;
+for(let i = 0; i < oils.length - 1; i++){
+  result += BigInt(oils[i]) * BigInt(city[i]);
 }
 console.log(String(result));
