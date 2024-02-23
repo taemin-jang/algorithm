500f9c2f90fb9ef3ac97719e8c8a1d0c3fd666ec
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2024-02-23 11:43:20 +0900

    [Silver I] Title: 회의실 배정, Time: 384 ms, Memory: 41996 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -1,15 +1,16 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let tc = +input[0];
-let arr = input.slice(1);
-arr = arr.map(v => v.split(" ").map(Number)).sort((a,b) => a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]);
+const n = +input[0];
+const newArr = input.slice(1).map(arr => arr.split(" ").map(Number)).sort((a,b) => a[1] === b[1] ? a[0] - b[0] : a[1] - b[1]);
 let count = 1;
-let [start, end] = arr[0];
-for(let i = 1; i < tc; i++){
-  if(end <= arr[i][0]){
-    count++;
-    end = arr[i][1];
-  }
+let [start, end] = newArr[0];
+
+for(let i = 1; i < n; i++) {
+    if(end <= newArr[i][0]){
+        end = newArr[i][1];
+        count++;
+    }
 }
+
 console.log(count);
