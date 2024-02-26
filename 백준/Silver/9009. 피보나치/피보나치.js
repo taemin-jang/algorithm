6b320de8a39cd6940b3ef0e272e955975757429b
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2024-02-26 11:30:16 +0900

    [Silver I] Title: 피보나치, Time: 200 ms, Memory: 11876 KB -BaekjoonHub

--- 백준/Silver/9009. 피보나치/피보나치.js
+++ 백준/Silver/9009. 피보나치/피보나치.js
@@ -1,43 +1,24 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let tc = +input[0];
+pibo = [];
+pibo.push(0);
+pibo.push(1);
+while (pibo[pibo.length- 1] < 1e9) pibo.push(pibo[pibo.length- 2] + pibo[pibo.length- 1]);
 
-let arr = Array(45).fill(0);
-function slowFib(n) {
-	if (n < 2) {
-    	return n; 
-    } 
-    
-    return fib(n-2) + fib(n-1) 
-} 
-
-//Generic Momoization Function 
-function memoize(fn) { 
-    const cache = {}; 
-    return function(...args) {
-    	if (cache[args]) { 
-            return cache[args]; 
-      } 
-      const result = fn.apply(this, args);
-      arr[args[0]] = result;
-      cache[args] = result; 
-      return result; 
-    } 
-} 
-
-const fib = memoize(slowFib);
-fib(44);
-for(let i = 1; i <= tc; i++){
-  let num = +input[i];
-  let result = [];
-  for(let j = arr.length - 1; j > 0; j--){
-    if(num >= arr[j]){
-      num -= arr[j];
-      result.push(arr[j]);
-      if(num === 0) break;
+const n = input[0];
+const result = []
+for(let i = 1; i <= n; i++){
+    let num = input[i];
+    let numArr = []
+    for(let j = pibo.length - 1; j >= 0; j--){
+        if(num === 0) break;
+        if(pibo[j] <= num){
+            numArr.push(pibo[j]);
+            num -= pibo[j];
+        }
     }
-  }
-  console.log(result.reverse().join(" "));
-  result = [];
+    result.push(numArr.sort((a,b) => a - b).join(" "));
 }
+
+console.log(result.join("\n"));
