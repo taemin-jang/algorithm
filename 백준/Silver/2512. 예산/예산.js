e69dcfb3f2d607ab6052c1e30b76abf2bce4ef01
Taemin Jang <82078896+taemin-jang@users.noreply.github.com>
2024-03-19 23:15:00 +0900

    [Silver II] Title: 예산, Time: 180 ms, Memory: 10180 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.js
+++ 백준/Silver/2512. 예산/예산.js
@@ -1,27 +1,21 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let n = +input[0];
-let arr = input[1].split(" ").map(Number);
-let m = +input[2];
-
+const n = +input[0];
+const arr = input[1].split(" ").map(Number);
+const total = +input[2];
 let start = 1;
-let end = arr.reduce((a, b) => Math.max(a,b));
-
+let end = Math.max(...arr);
 let result = 0;
-
 while(start <= end){
-  let mid = parseInt((start + end) / 2);
-  let total = 0;
-  for(let x of arr) {
-    total += Math.min(mid, x);
-  }
-  if(total <= m) {
-    result = mid;
-    start = mid + 1;
-  }else{
-    end = mid - 1;
-  }
+    const mid = parseInt((start + end) / 2);
+    const arrTotal = arr.reduce((acc, cur) => acc + Math.min(cur, mid), 0);
+    if(arrTotal <= total) {
+        result = mid;
+        start = mid + 1;
+    }else {
+        end = mid - 1;
+    }
 }
 
 console.log(result);
